8b38c0515bfc594115a7a210b6560a7628fe562a
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-11-17 01:38:32 +0900

    하 ts + vue는 DefineComponent를 해야하는건가? options여서 하는건가 이것때문에 애먹었네...

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header/>
+    <router-view/>
+    <Footer/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import Main from "@/components/Main.vue";
+
+@Options({
+  components: {
+    Header,Footer,Main
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
